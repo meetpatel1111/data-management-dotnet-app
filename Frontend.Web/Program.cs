2d8757ef24a9257a3ee06497d9a7af881f0fd43a
Meetkumar Patel <pmeet464@gmail.com>
2026-01-13 20:07:25 +0530

    Fix missing WebApplication namespace in frontend Program.cs

--- Frontend.Web/Program.cs
+++ Frontend.Web/Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseRouting();
+
 app.MapDefaultControllerRoute();
 
 app.Run();
